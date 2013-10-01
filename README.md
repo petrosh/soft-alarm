15f3d1c0b48c1817ade03575fe093ae10b3e3998
alessandro petrozzi <petrosh@petrosh.it>
2013-10-01 19:26:08 +0200

    first test

--- README.md
+++ README.md
@@ -1,37 +1,45 @@
 soft-alarm
 ==========
 
-soft alarm panel for agitated minds – now with edge parser
+soft alarm panel for agitated minds – now with edge parser [http://petrosh.github.io/soft-alarm/](http://petrosh.github.io/soft-alarm/)
 
 Plug-ins
 --------
 
 - [moment](https://github.com/moment/moment)  
 - [store.js](https://github.com/marcuswestin/store.js)  
-- [jquery.countdown](https://github.com/kemar/jquery.countdown)  jquery-1.10.1  
-- [favico.js](https://github.com/ejci/favico.js) jquery-1.10.1  
+- [jquery.countdown](https://github.com/kemar/jquery.countdown) (jquery-1.10.1)  
+- [favico.js](https://github.com/ejci/favico.js) (jquery-1.10.1)  
 
 Functions
--------
+---------
 
-- **SET** – Set an alarm *FAST*  
-- **TUNE** – Tune an alarm *FAST*  
-- **GET** – Gets what happened *FAST*
+**SET, PARSE, INPUT** – Set an alarm *FAST*, Parse & crunch lots of data formats, Parse text, *SORT IT* and store object.  
 
-Features
---------
+- Textarea parsed by rows in array  
+- Sort array and store  
+- Bind countdown loop  
 
-- **PARSE** – Parse & crunch lots of data formats  
-- **SOUND** – Sounds / mute / visual / favicon / link  
-- **TRACK** – Keep track of events  
-- **SKINS** – Of course  
+**TUNE, SOUND, ALARM** – Tune an alarm *FAST*, Sounds / mute / visual / favicon / link, Generate events required.  
 
-Modules
--------
+- Set Sound (none, 1 or 2)  
 
-- **INPUT** – Parse text, store object and *SORT IT*  
-- **ALARM** – Generate events required
-- **RUNTIME** – Tune / change / watch  
+**GET, TRACK, RUNTIME** – Gets what happened *FAST*, Keep track of events, Tune / change / watch.  
+
+- At expiry start count-up
+- Report original text (links)  
+
+**SKINS** – Maybe.  
+
+Contributing
+------------
+
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Added some feature'`)
+4. Make sure to add tests for it. This is important so I don't break it in a future version unintentionally.
+5. Push to the branch (`git push origin my-new-feature`)
+6. Create new Pull Request
 
 Inspiration
 ------------
--- soft-alarm.js
+++ soft-alarm.js
@@ -1,31 +1,104 @@
-$(document).ready(function(){
+$(document).ready(function(){ // jquery version 1.10.1
 	
-	// jquery
-	console.log('jquery version '+$().jquery);
-	
-	// moment
-	console.log(moment().format());
-	
-	// favico
+	// define favico
 	var favicon=new Favico({
-    	type : 'rectangle',
-		animation:'slide'
+    	type : 'rectangle'
 	});
+	
+	// return Array ( Settings, Current timers )
+	var softAlarm = init();
+	var settings = softAlarm[0];
+	var current = softAlarm[1];
+	var badges = 0;
+	
+	loop(current);
+
+	function loop(current){
+		for ( var i in current ) {
+			var id = current[i]['id'];
+			var string = current[i]['string'];
+			$('#clocks').prepend('<div><time>' + string + '</time> ' + id + '</div>');
+		};
+		$('time').countDown();
+		$('time').on('time.elapsed', function () {
+			liftOff(this);
+		});
+		return true;
+	}
 
 	// jquery.countdown
-	$('body').append('<div><time>00:01</time></div>');
-	$('time').countDown({
-		with_separators: true
+/* 	$('body').append('<div><time></time></div>');
+	var identifier=6;
+	$('time').countdown({
+		until: +5,
+		onExpiry: function endCount() {
+			liftOff(identifier);
+		},
+		format: 'DHMS',
+		compact: true
 	});
-    $('time').on('time.elapsed', function () {
-		favicon.badge(1);
-        var soundHandle = $('#soundHandle');
-        soundHandle.attr('src','lib/08 Communicator Beeps.mp3');
-        soundHandle.get(0).play();
-    });
-	
-	// store+json2
-	store.set('tags', ['javascript', 'localStorage', 'store.js'])
-	console.log("We've got " + store.get('tags').length + " tags here")
+	 */
+	function liftOff(e) {
+		
+		// get settings
+		var param = settings[0]['param'];
+		
+		// target element
+		//var div = '#' + id;
+		$( e ).css('color', 'red');
+		
+		// badges
+		badges = badges + 1;
+		favicon.badge( badges );
+		
+		// check sound settings
+		if( param == 0 ){
+			console.log(e);
+		}else{
+			var soundHandle = $('#soundHandle');
+			console.log(e)
+			soundHandle.attr('src', settings[ param ][ 'file' ]);
+			soundHandle.get(0).play();
+		}
+	};
 
 });
+
+function init(){
+	
+	// Check localStorage support
+	
+	if (!store.enabled) {
+		alert('Local storage is not supported by your browser. Please disable "Private Mode", or upgrade to a modern browser');
+		return;
+    }
+
+	// Check current timers
+	
+    var current = store.get('SAcurrent');
+	if( typeof current == 'undefined' ){
+		// Set default timers as current (sorted array of objects)
+		var data = [
+			{id: 'miao', string: '2013-10-01 19:21:22'},
+			{id: 'cairo', string: '00:00:10'},
+			{id: 'istambul', string: '00:01'}
+		];
+		store.set('SAcurrent',data);
+	}
+	
+	// Check settings
+	
+    var settings = store.get('SAsettings');
+	if( typeof settings == 'undefined' ){
+		// Set default settings
+		var data = [
+			{sound: 0, param: 1},
+			{sound: 1, file: 'lib/08 Communicator Beeps.mp3'},
+			{sound: 2, file: ''}
+		];
+		store.set('SAsettings',data);
+	}
+	
+	return Array( store.get('SAsettings'), store.get('SAcurrent') );
+
+}
